97b6cd4c5c56a02086d4e844a267827b41c482d6
李培汉 <lipeihan@chandashi.com>
2018-11-08 10:11:09 +0800

    perf: 修改 babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   presets: ['@babel/preset-env'],
   plugins: [
-    '@babel/plugin-transform-runtime',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: 2,
+        helpers: true,
+        regenerator: true,
+        useESModules: false
+      }
+    ],
     require('@babel/plugin-syntax-dynamic-import')
   ]
 };
