6bfefcc73502660d309f329d2c3b685c32ae2a51
lipeihan <291630202@qq.com>
2018-11-19 23:52:15 +0800

    style: style

--- src/utils/directives.js
+++ src/utils/directives.js
@@ -1,37 +1,35 @@
 import { throttle } from './index';
 import { cssSupport } from './dom';
 
-export default {
-  install(Vue) {
-    /**
-     * 示例: <div v-sticky="{ top: 0 }"><div>hello world</div></div>
-     */
-    Vue.directive('sticky', {
-      inserted(el, binding) {
-        const params = binding.value;
+export default function(Vue) {
+  /**
+   * 示例: <div v-sticky="{ top: 0 }"><div>hello world</div></div>
+   */
+  Vue.directive('sticky', {
+    inserted(el, binding) {
+      const params = binding.value;
 
-        if (cssSupport('position', 'sticky')) {
-          el.style['position'] = 'sticky';
-          el.style['top'] = params.top + 'px';
-          return;
-        }
-
-        el.parentNode.addEventListener(
-          'scroll',
-          throttle(() => {
-            const top = el.getBoundingClientRect().y;
-            const child = el.firstElementChild;
-
-            if (top <= params.top) {
-              child.style['position'] = 'fixed';
-              child.style['top'] = params.top + 'px';
-              child.style['z-index'] = 1;
-            } else {
-              child.style['position'] = 'static';
-            }
-          }, 100)
-        );
+      if (cssSupport('position', 'sticky')) {
+        el.style['position'] = 'sticky';
+        el.style['top'] = params.top + 'px';
+        return;
       }
-    });
-  }
-};
+
+      el.parentNode.addEventListener(
+        'scroll',
+        throttle(() => {
+          const top = el.getBoundingClientRect().y;
+          const child = el.firstElementChild;
+
+          if (top <= params.top) {
+            child.style['position'] = 'fixed';
+            child.style['top'] = params.top + 'px';
+            child.style['z-index'] = 1;
+          } else {
+            child.style['position'] = 'static';
+          }
+        }, 100)
+      );
+    }
+  });
+}
