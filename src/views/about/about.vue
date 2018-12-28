4d669b298b9d0479deee572cb689d8f57caaf1c1
李培汉 <lipeihan@chandashi.com>
2018-12-28 16:48:12 +0800

    style: 新增 border-1px bg-image 方法

--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -2,6 +2,8 @@
   <div class="about">
     <h1 @click="$refs.popup.open()">{{name}}</h1>
     <popup ref="popup">123</popup>
+    <div class="rank"></div>
+    <div class="block"></div>
   </div>
 </template>
 
@@ -19,3 +21,16 @@ export default {
   }
 };
 </script>
+
+<style lang="stylus" scoped>
+.about
+  .rank
+    size: 20px
+    background-size: 100% 100%
+    bg-image("~@/assets/imgs/rank")
+
+  .block
+    size: 200px
+    position: relative
+    border-1px()
+</style>
